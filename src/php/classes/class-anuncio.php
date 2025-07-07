c9b6cc785ef537ccf660d061df5c5e95e0299a54
Otavio da Silva Pacheco <otaviopacheco@adm.cavg>
2025-07-07 12:03:45 -0300

    criação da classe anuncio

--- src/php/classes/class-anuncio.php
+++ src/php/classes/class-anuncio.php
@@ -6,7 +6,7 @@
         private $data_de_exclusao;
         private $conexao;
 
-        public function __construct($anuncio_id, $anuncio_valor, $data_de_criação, $data_de_exclusao, $conexao){
+        public function __construct($anuncio_id, $anuncio_valor, $data_de_criacao, $data_de_exclusao, $conexao){
 
             $this->anuncio_id = $anuncio_id;
             $this->anuncio_valor = $anuncio_valor;
@@ -14,7 +14,9 @@
             $this->data_de_exclusao = $data_de_exclusao;
             $this->conexao = $conexao;
 
-            
+        }
+        public function criaAnuncio(){
+            $sql = "INSERT INTO anuncio (anuncio_valor, data_de_criacao, data_de_exclusao) VALUES ???" ;
 
 
 
