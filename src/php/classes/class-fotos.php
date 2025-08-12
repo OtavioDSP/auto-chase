2df2177dff1c1fb3f4865217ef81c8e89ae4ddcc
Otavio da Silva Pacheco <otaviopacheco@adm.cavg>
2025-08-12 12:00:42 -0300

    alterações na classe

--- src/php/classes/class-fotos.php
+++ src/php/classes/class-fotos.php
@@ -2,13 +2,16 @@
     class Fotos{
 
         private $fotos_id;
-        private $foto;
+        private $foto_caminho;
+        private $foto_user;
+        private $data_de_upload;
         private $conexao;
 
-        public function __construct($fotos_id, $foto, $conexao){
+        public function __construct($fotos_id, $foto_caminho, $foto_user, $data_de_upload, $conexao){
 
             $this->fotos_id = $fotos_id;
-            $this->foto = $foto;
+            $this->foto_user = $foto_user;
+            $this->foto_caminho = $foto_caminho;
             $this->conexao = $conexao;
 
         }
