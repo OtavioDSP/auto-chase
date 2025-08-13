cfb6532f26466da60b1536fcc91d834cb6592f75
Otavio da Silva Pacheco <otaviopacheco@adm.cavg>
2025-08-13 11:41:26 -0300

    iniciando arquivos de configuração para testes com banco de dados real

---
+++ src/config/db/connect.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$host ="localhost";
+$password ="";  
+$user ="root"; 
+$db = "autochase";
+$conexao = new mysqli($host, $user, $password, $db);
+
+
+if($conexao->connect_errno){
+    echo "Falha ao conectar MySQL".$conexao->connect_error;
+    exit();
+}
+
+?>
+
+?>
