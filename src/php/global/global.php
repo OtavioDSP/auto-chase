be2dbb17e4debba8e1bd14b44aeb9a0296d1c840
Otavio <aapo5719@gmail.com>
2025-08-25 22:39:11 -0300

    inicio dos testes em banco

---
+++ src/php/global/global.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'db.php';
+include_once 'classes/class-usuario.php';
+
+$criar_conta = $_POST['criar_conta'];
+
+if(isset($criar_conta)){
+    $usuario_nome = $_POST['usuario_nome'];
+    $usuario_senha = $_POST['usuario_senha'];
+    $usuario_email = $_POST['usuario_email'];
+    $usuario_telefone = $_POST['usuario_telefone'];
+    $usuario_endereco = $_POST['usuario_endereco'];
+    $doc_cpf_cnpj = $_POST['doc_cpf_cnpj'];
+
+
+    $usuario = new Usuario($usuario_nome, $usuario_senha, $usuario_email, $usuario_telefone, $usuario_endereco, $doc_cpf_cnpj);
+    $usuario->insereUsuario();
+
+    echo "Conta criada com sucesso!";
+}
+
+
+
+?>
