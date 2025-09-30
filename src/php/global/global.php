3faf75b0cb8bc3f769deddf90fee5f8ea79df0c2
OtavioDSP <aapo5719@gmail.com>
2025-09-30 12:04:01 -0300

    att

--- src/php/global/global.php
+++ src/php/global/global.php
@@ -61,8 +61,25 @@ if(isset($_POST['criar_conta'])){
    
 
     
-}
-if(isset($_POST['editar_modelo'])){
+}if(isset($_POST['editar_usuario'])){
+    
+    $usuario_nome = $_POST['usuario_nome'];
+    $usuario_senha = $_POST['usuario_senha'];
+    $usuario_email = $_POST['usuario_email'];
+    $usuario_telefone = $_POST['usuario_telefone'];
+    $usuario_endereco = $_POST['usuario_endereco'];
+    $doc_cpf_cnpj = $_POST['usuario_doc_cpf_cnpj'];
+    
+    $doc_formatado = formatarDocumento($doc_cpf_cnpj);
+    
+    echo $doc_formatado;
+
+    $usuario = new Usuario("", $usuario_nome, $usuario_email, $usuario_senha, $usuario_endereco, $usuario_telefone, $doc_formatado, "", $conexao);
+    $usuario->editarUsuario();
+
+    
+    
+}if(isset($_POST['editar_modelo'])){
     
     $modelo_id = intval($_POST['modelo_id']);
     $modelo_desc = $_POST['modelo_desc'];
