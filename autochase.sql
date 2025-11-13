0846f9d370666abdd82cfde075c1a0e25eaccbbb
andreicuruja <andreicuruja@outlook.com>
2025-11-13 00:38:05 -0300

    banco novo p develop

--- autochase.sql
+++ autochase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 04, 2025 at 03:20 AM
+-- Generation Time: Nov 12, 2025 at 04:15 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.26
 
@@ -48,13 +48,21 @@ INSERT INTO `anuncio` (`anuncio_id`, `anuncio_valor`, `anuncio_data_de_alteracao
 (3, 140000.00, '2025-11-02 18:15:14', '2025-11-02 18:15:14', 'Fusion 2022', 'INATIVO', 1, 3),
 (4, 55000.00, '2025-11-02 18:15:14', '2025-11-02 18:15:14', 'Gol 2023', 'VENDIDO', 2, 4),
 (6, 8000.00, '2025-11-02 19:11:28', '2025-11-02 19:11:28', 'vovorolla lindo', 'ATIVO', 1, 7),
-(8, 1.00, '2025-11-02 19:21:35', '2025-11-02 19:21:35', 'TESTE', 'ATIVO', 1, 9),
+(8, 2000.00, '2025-11-07 22:22:13', '2025-11-02 19:21:35', 'RS6', 'ATIVO', 1, 9),
 (9, 11111.00, '2025-11-02 22:27:39', '2025-11-02 22:27:39', '1aaaaa', 'ATIVO', 1, 10),
 (10, 1111.00, '2025-11-02 22:33:14', '2025-11-02 22:33:14', '1111', 'ATIVO', 1, 11),
 (11, 11111.00, '2025-11-02 22:38:08', '2025-11-02 22:38:08', '1111', 'ATIVO', 1, 12),
 (12, 111111.00, '2025-11-02 23:04:16', '2025-11-02 23:04:16', 'TESTE', 'ATIVO', 1, 13),
 (13, 1.00, '2025-11-03 00:03:20', '2025-11-03 00:03:20', 'deve ter so 1', 'ATIVO', 1, 15),
-(14, 1.00, '2025-11-03 00:04:27', '2025-11-03 00:04:27', '', 'ATIVO', 1, 16);
+(14, 10000.00, '2025-11-08 17:27:06', '2025-11-03 00:04:27', 'Audi teste', 'INATIVO', 1, 16),
+(15, 2222.00, '2025-11-07 20:00:00', '2025-11-07 20:00:00', '2222', 'ATIVO', 1, 17),
+(16, 55555.00, '2025-11-07 20:01:01', '2025-11-07 20:01:01', 'gggg', 'ATIVO', 1, 18),
+(17, 4555566.00, '2025-11-07 20:03:11', '2025-11-07 20:03:11', 'ssss', 'ATIVO', 1, 19),
+(18, 55555.00, '2025-11-07 20:08:09', '2025-11-07 20:08:09', 'sadassda', 'ATIVO', 1, 20),
+(19, 55555.00, '2025-11-07 20:11:03', '2025-11-07 20:11:03', 'sadassda', 'ATIVO', 1, 21),
+(20, 1111.00, '2025-11-08 20:18:25', '2025-11-08 20:18:25', 'aaaa', 'ATIVO', 1, 22),
+(21, 1111.00, '2025-11-08 20:19:52', '2025-11-08 20:19:52', '111', 'ATIVO', 1, 23),
+(22, 33333.00, '2025-11-12 02:30:52', '2025-11-08 21:34:53', 'teste civic', 'ATIVO', 6, 24);
 
 -- --------------------------------------------------------
 
@@ -75,7 +83,8 @@ INSERT INTO `chassi` (`chassi_id`, `chassi_desc`) VALUES
 (1, 'SW'),
 (2, 'Sedan'),
 (3, 'SUV'),
-(4, 'Pickup');
+(4, 'Pickup'),
+(6, 'Coupé');
 
 -- --------------------------------------------------------
 
@@ -118,7 +127,8 @@ INSERT INTO `cor` (`cor_id`, `cor_desc`) VALUES
 (2, 'Branco'),
 (3, 'Prata'),
 (4, 'Vermelho'),
-(5, 'Verde');
+(5, 'Verde'),
+(9, 'Amarelo');
 
 -- --------------------------------------------------------
 
@@ -140,9 +150,9 @@ CREATE TABLE `imagem` (
 INSERT INTO `imagem` (`imagem_id`, `imagem_url`, `fk_anuncio_id`, `imagem_data_insercao`) VALUES
 (1, '/images/corolla_frente.jpg', 1, '2025-11-02 18:15:14'),
 (2, '/images/corolla_interior.jpg', 1, '2025-11-02 18:15:14'),
-(3, '/images/civic_lado.jpg', 2, '2025-11-02 18:15:14'),
+(3, '../../uploads/104616fd-c7b4-4a60-8a65-b9b6f5ec4c6f_9e60e5d4-e0ad-438e-b45c-af931241dd7f.webp', 2, '2025-11-02 18:15:14'),
 (4, '/images/fusion_completo.jpg', 3, '2025-11-02 18:15:14'),
-(6, '../../uploads/6907af3f27851_audi-rs6-4.0-v8-tfsi-mhev-avant-performance-tiptronic-wmimagem08305069556.jpg', 8, '2025-11-02 19:21:35'),
+(6, '../../uploads/Marca 01.jpg', 8, '2025-11-02 19:21:35'),
 (7, '../../uploads/5396239.png', 9, '2025-11-02 22:27:39'),
 (8, 'E:\\laragon\\www\\TCC\\auto-chase\\src\\php\\global/../../uploads/6907dc2a8a434-cosmic-2nog8px0dq0y2q24.jpg', 10, '2025-11-02 22:33:14'),
 (9, 'E:\\laragon\\www\\TCC\\auto-chase\\src\\php\\global/../../uploads/6907dc2a98787-images.jpeg', 10, '2025-11-02 22:33:14'),
@@ -150,16 +160,16 @@ INSERT INTO `imagem` (`imagem_id`, `imagem_url`, `fk_anuncio_id`, `imagem_data_i
 (11, 'E:\\laragon\\www\\TCC\\auto-chase\\src\\php\\global/../../uploads/6907dc2abd09c-i-made-the-praying-meme-v0-wjh3bsyxlhse1.webp', 10, '2025-11-02 22:33:14'),
 (12, 'E:\\laragon\\www\\TCC\\auto-chase\\src\\php\\global/../../uploads/6907dc2ac93ce-2203422.png', 10, '2025-11-02 22:33:14'),
 (13, 'E:\\laragon\\www\\TCC\\auto-chase\\src\\php\\global/../../uploads/6907dc2ad78a1-5396239.png', 10, '2025-11-02 22:33:14'),
-(14, 'uploads/6907dd50a8ac8-5396239.png', 11, '2025-11-02 22:38:08'),
-(15, 'uploads/6907dd50c1171-21517.png', 11, '2025-11-02 22:38:08'),
-(16, 'uploads/6907dd50d4719-208939-afinal-o-curso-de-agronomia-e-o-ideal-para-mim.jpg', 11, '2025-11-02 22:38:08'),
-(17, 'uploads/6907dd50e0b13-126238~1.JPG', 11, '2025-11-02 22:38:08'),
-(18, 'uploads/6907dd50ecd91-mulher-na-agricultura.jpg', 11, '2025-11-02 22:38:08'),
 (19, 'uploads/6907e370c9849-cosmic-2nog8px0dq0y2q24.jpg', 12, '2025-11-02 23:04:16'),
 (20, 'uploads/6907e370ebc25-images.jpeg', 12, '2025-11-02 23:04:16'),
 (21, 'uploads/6907e3711636d-audi-rs6-4.0-v8-tfsi-mhev-avant-performance-tiptronic-wmimagem08305069556.jpg', 12, '2025-11-02 23:04:17'),
 (22, '../../uploads/audi-rs6-4.0-v8-tfsi-mhev-avant-performance-tiptronic-wmimagem08305069556.jpg', 13, '2025-11-03 00:03:20'),
-(23, '../../uploads/images.jpeg', 14, '2025-11-03 00:04:27');
+(25, 'uploads/Marca 02.jpg', 18, '2025-11-07 20:08:09'),
+(26, '../../uploads/Marca 02.jpg', 19, '2025-11-07 20:11:03'),
+(27, '../../uploads/imagemlegaldeteaaa.webp', 11, '2025-11-08 17:23:22'),
+(28, '../../uploads/imagem.webp', 14, '2025-11-08 17:27:06'),
+(29, 'src/uploads/audi-rs6-4.0-v8-tfsi-mhev-avant-performance-tiptronic-wmimagem08305069556.jpg', 21, '2025-11-08 20:19:52'),
+(30, 'src/uploads/images.jpeg', 22, '2025-11-08 21:34:53');
 
 -- --------------------------------------------------------
 
@@ -182,7 +192,8 @@ INSERT INTO `marca` (`marca_id`, `marca_desc`) VALUES
 (3, 'Ford'),
 (4, 'Volkswagen'),
 (6, 'Ferrari'),
-(9, 'Audi');
+(9, 'Audi'),
+(10, 'Mazda ');
 
 -- --------------------------------------------------------
 
@@ -207,7 +218,8 @@ INSERT INTO `modelo` (`modelo_id`, `modelo_desc`, `modelo_valor_fipe`, `fk_Marca
 (3, 'Corolla', 95000.00, 1),
 (4, 'Gol', 45000.00, 4),
 (5, 'Fusion', 100000.00, 3),
-(7, 'RS6', 111111.00, 9);
+(7, 'RS6', 111111.00, 9),
+(8, 'RX7', 70000.00, 10);
 
 -- --------------------------------------------------------
 
@@ -232,7 +244,9 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`usuario_id`, `usuario_nome`, `usuario_senha`, `usuario_nivel_de_acesso`, `usuario_email`, `usuario_telefone`, `usuario_endereco`, `usuario_doc_cpf_cnpj`) VALUES
 (1, 'Administrador', 'admin123', 'ADMIN', 'admin@example.com', '999999999', 'Rua 1, 123', '12345678901'),
-(2, 'Usuário Teste', 'usuario123', 'USUARIO', 'usuario@example.com', '988888888', 'Rua 2, 456', '98765432100');
+(2, 'Usuário Teste', 'usuario123', 'USUARIO', 'usuario@example.com', '988888888', 'Rua 2, 456', '98765432100'),
+(5, 'teste', '$2y$10$VSLKDlcFhFGVChLp6XiDYuSJNudRWcj90G0NySoTaFQhMZteIWWnm', 'ADMIN', 'teste@gmail.com', 'teste', '2222222', '111.131.111-11'),
+(6, 'usuario teste', '$2y$10$USfTwu700aUcQ.ZS.vUT..pz6aZgMSbQ.zw5QjM4keyJZIWD9be1G', 'USUARIO', 'teste1@gmail.com', '6666666', 'rua ', '111.144.111-11');
 
 -- --------------------------------------------------------
 
@@ -262,14 +276,22 @@ INSERT INTO `veiculo` (`veiculo_id`, `veiculo_quilometragem`, `fk_Chassi_id`, `f
 (4, 0.00, 4, 4, 4, 4, 'Versão 2023', 2023),
 (7, 9999.00, 2, 2, 1, 4, 'XEi', 1995),
 (8, 9999.00, 2, 2, 1, 4, 'XEi', 1995),
-(9, 399.00, 1, 1, 7, 1, 'Avant MHEV ', 2022),
+(9, 3400.00, 2, 2, 1, 1, 'Avant MHEV Diesel', 2021),
 (10, 111.00, 1, 1, 1, 1, '1111', 1111),
 (11, 1111.00, 1, 1, 1, 1, '1111', 1111),
 (12, 1111.00, 1, 1, 2, 1, '1111', 1111),
 (13, 1111.00, 1, 1, 1, 1, 'TESTE ', 1111),
 (14, 111.00, 1, 1, 1, 1, '111', 1111),
 (15, 111.00, 1, 1, 1, 1, '111', 1111),
-(16, 20.00, 2, 4, 7, 2, '20', 2050);
+(16, 200000.00, 1, 5, 7, 2, '11', 2015),
+(17, 111.00, 1, 1, 1, 1, 'aaa', 1111),
+(18, 888.00, 1, 1, 1, 1, 'hhhh', 8888),
+(19, 5555.00, 1, 1, 1, 1, 'ssdsd', 5555),
+(20, 3333.00, 1, 1, 1, 1, 'asda', 6666),
+(21, 3333.00, 1, 1, 1, 1, 'asda', 6666),
+(22, 111.00, 1, 1, 2, 1, '1111', 1111),
+(23, 111.00, 1, 1, 2, 1, 'saasa', 1111),
+(24, 1111.00, 1, 1, 2, 1, 'ssss', 2011);
 
 --
 -- Indexes for dumped tables
@@ -346,13 +368,13 @@ ALTER TABLE `veiculo`
 -- AUTO_INCREMENT for table `anuncio`
 --
 ALTER TABLE `anuncio`
-  MODIFY `anuncio_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `anuncio_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `chassi`
 --
 ALTER TABLE `chassi`
-  MODIFY `chassi_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `chassi_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `combustivel`
@@ -364,37 +386,37 @@ ALTER TABLE `combustivel`
 -- AUTO_INCREMENT for table `cor`
 --
 ALTER TABLE `cor`
-  MODIFY `cor_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `cor_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `imagem`
 --
 ALTER TABLE `imagem`
-  MODIFY `imagem_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `imagem_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `marca_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `marca_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `modelo`
 --
 ALTER TABLE `modelo`
-  MODIFY `modelo_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `modelo_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `usuario_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `usuario_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `veiculo`
 --
 ALTER TABLE `veiculo`
-  MODIFY `veiculo_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `veiculo_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
