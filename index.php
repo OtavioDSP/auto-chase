e76ee3ea5dd493a358df3fd54dedf76dfadffbb0
OtavioDSP <aapo5719@gmail.com>
2025-12-11 10:22:14 -0300

    modificações finais

--- index.php
+++ index.php
@@ -71,10 +71,10 @@ $_SERVER['SERVER_NAME']== "localhost" ? $URL= "/tcc/auto-chase/" : $URL= "/autoc
     <?php
     // Includes das classes movidos para cima,
     // para que eAdmin() e os filtros públicos funcionem.
-    include('/src/config/db/connect.php');
-    include("/src/php/classes/class-usuario.php");
-    include("/src/php/classes/class-veiculo.php");
-    include("/src/php/classes/class-modelo.php");
+    include("./src/config/db/connect.php");
+    include("./src/php/classes/class-usuario.php");
+    include("./src/php/classes/class-veiculo.php");
+    include("./src/php/classes/class-modelo.php");
     include("./src/php/classes/class-marca.php");
     include("./src/php/classes/class-cor.php");
     include("./src/php/classes/class-chassi.php");
